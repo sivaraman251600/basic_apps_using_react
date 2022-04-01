59eea0fbf0ffe7b8bf935faf181cbb790bf8a522
K <251600@NTTDATA.COM>
2022-04-01 16:30:07 +0530

    Updating the lifecycle excercise

--- trainingthree/src/App.js
+++ trainingthree/src/App.js
@@ -5,6 +5,8 @@ import PracticeTwo from "./components/moduleone";
 import Supplier from "./components/prop";
 import StudentDetails from "./components/stat";
 import Counter from "./components/counter";
+import LifeA from "./components/lifecycle";
+import UpdatingA from "./components/updatinglifeA";
 
 function App() {
   return (
@@ -12,9 +14,14 @@ function App() {
       <PracticeOne />
       <PracticeTwo />
       <Supplier />
+      <hr/>
       <StudentDetails />
       <hr/>
       <Counter />
+      <hr/>
+      <LifeA />
+      <hr/>
+      <UpdatingA />
     </div>
   );
 }
